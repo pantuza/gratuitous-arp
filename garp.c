0e6cf8f931983444aef0a57ab6005e933b63e7e6
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-04 01:11:37 -0200

    Create the initial project structure

---
+++ garp.c
@@ -0,0 +1,44 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  garp.c
+ *
+ *    Description:  Simple implementation of a gratuitous ARP packet.
+ *
+ *        Created:  02/03/2017 09:30:35 PM
+ *
+ *         Author:  Gustavo Pantuza
+ *   Organization:  Computer Science Community
+ *
+ * ============================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void
+usage ()
+{
+    fprintf(stdout,
+        "Usage: garp iface addr\n"
+        "Example: garp eth0 192.168.1.42\n"
+    );
+}
+
+int
+main (int argc, char* argv[])
+{
+    if(argc != 3) {
+        fprintf(stderr, "Error: Wrong number of arguments %d\n\n", argc);
+        usage();
+        return EXIT_FAILURE;
+    }
+
+    fprintf(stdout, "Sending gratuitous ARP..\n");
+
+    /* Do stuff */
+
+    return EXIT_SUCCESS;
+}
