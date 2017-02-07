59fc56af47500aedca107a9cbaf06e060e3deea3
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2017-02-06 23:30:04 -0200

    BugFix: Unknown storage size solved by including net/if.h

--- garp.c
+++ garp.c
@@ -13,7 +13,6 @@
  * ============================================================================
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,6 +24,7 @@
 #include <arpa/inet.h>
 
 #include <linux/if_ether.h>
+#include <net/if.h>
 
 #include "garp.h"
 #include "garp_config.h"
